be4062be4adb7cf5e3df8ca66141704dbe85fc47
wowmobilya <www.wowmobilya@gmail.com>
2026-05-09 19:30:34 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    WOW MOBİLYA — Service Worker
    ⚠️ غيّر VERSION عند كل تحديث لـ index.html
 ============================================================ */
-const VERSION    = 'v64';
+const VERSION    = 'v65';
 const CACHE_NAME = `wow-mobilya-${VERSION}`;
 
 const CORE_ASSETS = ['./', './index.html'];
